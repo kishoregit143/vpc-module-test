122c7afdc91064824dab04545372596957d58421
kishore <pagadalamokisthu@gmail.com>
2026-02-17 12:16:19 +0530

    terraform

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ module "vpc" {
     private_subnet_cidrs = var.private_subnet_cidrs
     #database
     database_subnet_cidrs = var.database_subnet_cidrs
+    # is_peering_required = true
     
 
  }
